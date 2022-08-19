3b00924114430faa88ee60a0527012bd51b179ea
supbose <yshxw@qq.com>
2022-08-19 20:47:53 +0800

    保存

---
+++ docs/.vuepress/components/indexTime.vue
@@ -0,0 +1,35 @@
+    <template>
+        <div>
+            
+        </div>
+    </template>
+<script>
+export default {
+	data() {
+		return {}
+	},
+	created() {
+		// console.log('object :>> ', `object`)
+		this.userWebUrl()
+	},
+	mounted() {},
+	methods: {
+		userWebUrl() {
+			let language = navigator.language
+			let domain = document.domain
+			if (domain == 'cn.yiruan.wang' || domain == 'localhost') {
+				// console.log(domain)
+			} else {
+				// console.log(`1111`)
+				if (language == 'zh-CN') {
+					location.href = '//cn.yiruan.wang'
+				} else {
+					location.href = '//www.yiruan.wang'
+				}
+			}			
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss"></style>
