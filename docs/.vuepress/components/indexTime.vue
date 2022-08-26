38495fccf9fd529aa5a2cfb5e685b2adcbbf318c
supbose <yshxw@qq.com>
2022-08-26 11:41:07 +0800

    域名判断

--- docs/.vuepress/components/indexTime.vue
+++ docs/.vuepress/components/indexTime.vue
@@ -1,19 +1,36 @@
-<template>
-	<div class="timesindex"></div>
+<template>	
+	<div class="timesindex" style="display: none"></div>
 </template>
 <script>
 export default {
 	data() {
-		return {}
+		return {
+			cnHost: '//cn.yiruan.wang',
+			Host:'//www.yiruan.wang'
+		}
 	},
-	created() {
-		// console.log('object :>> ', `object`)
-		this.userWebUrl()
+	// created() {
+	// 	// console.log('object :>> ', `object`)
+	// 	this.userWebUrl()
+	// },
+	mounted() {
+		let language = navigator.language
+			let domain = document.domain
+			if (domain == 'cn.yiruan.wang' || domain == 'localhost') {
+				console.log(domain)
+			} else {
+				console.log(`1111`)
+				if (language == 'zh-CN') {
+					location.href = '//cn.yiruan.wang'
+				} else {
+					location.href = '//www.yiruan.wang'
+				}
+			}
+
 	},
-	mounted() {},
 	methods: {
 		userWebUrl() {
-			if (typeof global.navigator === 'undefined') global.navigator = {};
+			// if (typeof global.navigator === 'undefined') global.navigator = {};
 			let language = navigator.language
 			let domain = document.domain
 			if (domain == 'cn.yiruan.wang' || domain == 'localhost') {
